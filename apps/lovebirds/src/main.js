afe30b82e82556e739cfd7334dcea9bb1da89b09
yuning-dev <yuning.eng@gmail.com>
2024-03-19 15:29:52 +0000

    add elements of the profile page; add heart icon

--- apps/lovebirds/src/main.js
+++ apps/lovebirds/src/main.js
@@ -2,6 +2,13 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import axios from 'axios'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+import { faHeart } from '@fortawesome/free-regular-svg-icons'
+
+library.add(faHeart)
+
 import App from './App.vue'
 import router from './router'
 
@@ -9,6 +16,8 @@ import router from './router'
 // axios.defaults.baseURL = ''
 
 const app = createApp(App)
+
+app.component('font-awesome-icon', FontAwesomeIcon)
 
 app.use(createPinia())
 app.use(router)
--- apps/lovebirds/src/pages/Profile/Profile.vue
+++ apps/lovebirds/src/pages/Profile/Profile.vue
@@ -1,6 +1,19 @@
 <template>
-    <div :class="$style.wrapper" style="background-image: url('meadows-background.jpg')">
-        <div :class="$style.textWrapper">{{ user }}</div>
+    <div :class="$style.wrapper" style="background-image: url('profile-background.jpg')">
+        <div :class="$style.contentWrapper">
+            <img :class="$style.profileImg" src="../../../public/profile-pic.jpg" height="450">
+            <div :class="$style.textWrapper">
+                <div :class="$style.name">Pablo Fernandez</div>
+                <div :class="$style.age">21</div>
+                <div :class="$style.tagInfoContainer">
+                    <div :class="$style.tag1">I love </div><div :class="$style.info"> posing, surfing and gaming</div>
+                </div>
+                <div :class="$style.tagInfoContainer">
+                    <div :class="$style.tag1">I am </div><div :class="$style.info"> extroverted, cheeky and charming</div>
+                </div>
+                <button :class="$style.heartBtn"><font-awesome-icon icon="fa-regular fa-heart" size="lg" /></button>
+            </div>
+        </div>
     </div>
 
 </template>
