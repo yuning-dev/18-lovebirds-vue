ab0195d8d8d8b76534a8f0c79aae2c6f59026ef5
yuning-dev <yuning.eng@gmail.com>
2024-03-26 11:30:46 +0000

    style chat window component

--- apps/lovebirds/src/components/ChatWindow/ChatWindow.vue
+++ apps/lovebirds/src/components/ChatWindow/ChatWindow.vue
@@ -3,16 +3,18 @@
         <div :class="$style.chatHeader">
             <div :class="$style.chatee">Jess</div>
             <div :class="$style.headerIcons">
-                <div :class="$style.headerIcon"><font-awesome-icon icon="fa-solid fa-window-minimize" size="lg"/></div>
+                <div :class="[$style.headerIcon, $style.minimiseBtn]"><font-awesome-icon icon="fa-solid fa-window-minimize" size="lg"/></div>
                 <div :class="$style.headerIcon"><font-awesome-icon icon="fa-solid fa-xmark" size="lg"/></div>
             </div>
         </div>
         <div :class="$style.chatWindow">
-            <template v-for="message in messages">
-                <div :class="$style.msgBubble">{{ message.content }}</div>
-            </template>
+            <div :class="$style.chatBody">
+                <template v-for="message in messages">
+                    <div :class="$style.bubble, {[$style.leftBubble]: isLeft, [$style.rightBubble]: isRight}">{{ message.content }}</div>
+                </template>
+            </div>
             <div :class="$style.bottomBar">
-                <input type="text" v-model="content" :class="$style.chatInput"/>
+                <input type="text" v-model="content" :class="$style.chatInput" v-on:keyup.enter="sendBtnClicked"/>
                 <button :class="$style.sendBtn" @click="sendBtnClicked">Send</button>
             </div>
         </div>
@@ -20,6 +22,10 @@
 </template>
 
 <script>
+import { mapWritableState } from 'pinia'
+
+import { useUserStore } from '@/stores/user'
+
 export default{
     name: 'ChatWindow',
     data() {
@@ -28,13 +34,20 @@ export default{
             content: ''
         }
     },
+    // computed: {
+    //     ...mapWritableState('use')
+    // },
     methods: {
         sendBtnClicked() {
+            if (!this.content) {
+                return
+            }
             const message = {
                 content: this.content,
                 author: ''
             }
             this.messages.push(message)
+            this.content = ''
         }
     }
 }
